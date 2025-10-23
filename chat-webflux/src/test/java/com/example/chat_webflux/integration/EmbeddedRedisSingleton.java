2781824255757040426441c1450ed9ff0c999dad
hppro2024\smcore2024 <klpark@smcore.co.kr>
2025-10-23 18:06:39 +0900

    docs(webflux): EmbeddedRedisSingleton 주석 문구 추가

--- chat-webflux/src/test/java/com/example/chat_webflux/integration/EmbeddedRedisSingleton.java
+++ chat-webflux/src/test/java/com/example/chat_webflux/integration/EmbeddedRedisSingleton.java
@@ -5,6 +5,8 @@ import redis.embedded.RedisServer;
 /**
  * Embedded Redis Server
  * 전체 테스트 클래스가 캐싱 대상에 상관없이 공유하기 위해 Singleton 사용
+ * @SpringBootTest만 붙은 테스트 클래스 간 전환 시에도, @MockBean처럼 컨텍스트에 영향을
+ * 주는 경우 캐싱 대신 컨텍스트가 재로딩되기 때문에 싱글톤으로 관리하여 중복 실행 문제를 방지
  */
 public class EmbeddedRedisSingleton {
 
