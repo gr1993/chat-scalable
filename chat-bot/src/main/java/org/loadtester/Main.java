8ddf36afba9647574bad7b4fcffad83a2e56dd51
hppro2024\smcore2024 <klpark@smcore.co.kr>
2025-10-30 11:14:50 +0900

    feat(bot): WebSocketClient 클래스 connect 메서드 구현

--- chat-bot/src/main/java/org/loadtester/Main.java
+++ chat-bot/src/main/java/org/loadtester/Main.java
@@ -2,44 +2,22 @@ package org.loadtester;
 
 import org.loadtester.config.ConfigLoader;
 import org.loadtester.dto.LoadTestConfig;
-
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.WebSocket;
-import java.util.concurrent.CompletionStage;
+import org.loadtester.service.WebSocketClient;
 
 public class Main {
     private final static LoadTestConfig config = ConfigLoader.load("config.json");
 
     public static void main(String[] args) {
-        HttpClient client = HttpClient.newHttpClient();
-        WebSocket webSocket = client.newWebSocketBuilder()
-                .buildAsync(URI.create("ws://localhost:8080/ws/chat"), new WebSocket.Listener() {
-                    @Override
-                    public void onOpen(WebSocket webSocket) {
-                        System.out.println("✅ Connected to server");
-                        webSocket.sendText("Hello from client!", true);
-                        WebSocket.Listener.super.onOpen(webSocket);
-                    }
+        WebSocketClient webSocketClient = new WebSocketClient(config.getWebSocketEndpoint());
+        webSocketClient.connect();
 
-                    @Override
-                    public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
-                        System.out.println("📩 Received: " + data);
-                        return WebSocket.Listener.super.onText(webSocket, data, last);
-                    }
+        try {
+            Thread.sleep(10000);
+            String test = webSocketClient.getSessionId();
+            String test2 = "";
 
-                    @Override
-                    public CompletionStage<?> onClose(WebSocket webSocket, int statusCode, String reason) {
-                        System.out.println("❌ Closed: " + reason);
-                        return WebSocket.Listener.super.onClose(webSocket, statusCode, reason);
-                    }
+        } catch (Exception ex) {
 
-                    @Override
-                    public void onError(WebSocket webSocket, Throwable error) {
-                        System.err.println("⚠️ Error: " + error.getMessage());
-                    }
-                }).join();
-
-        webSocket.sendText("", false);
+        }
     }
 }
---
+++ chat-bot/src/main/java/org/loadtester/service/WebSocketClient.java
@@ -0,0 +1,61 @@
+package org.loadtester.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.WebSocket;
+import java.util.Map;
+import java.util.concurrent.CompletionStage;
+
+@Getter
+public class WebSocketClient {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final String url;
+    private WebSocket webSocket;
+    private String sessionId;
+
+    public WebSocketClient(String url) {
+        this.url = url;
+    }
+
+    public void connect() {
+        HttpClient client = HttpClient.newHttpClient();
+        webSocket = client.newWebSocketBuilder()
+                .buildAsync(URI.create(url), new WebSocket.Listener() {
+                    @Override
+                    public void onOpen(WebSocket webSocket) {
+                        System.out.println("웹소켓 연결 성공");
+                        WebSocket.Listener.super.onOpen(webSocket);
+                    }
+
+                    @Override
+                    public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
+                        System.out.println("메시지 수신: " + data);
+                        try {
+                            Map<String, Object> map = mapper.readValue(String.valueOf(data), Map.class);
+                            if ("session-info".equals(map.get("type"))) {
+                                sessionId = (String)map.get("sessionId");
+                            }
+                        } catch (JsonProcessingException e) {
+                            throw new RuntimeException(e);
+                        }
+                        return WebSocket.Listener.super.onText(webSocket, data, last);
+                    }
+
+                    @Override
+                    public CompletionStage<?> onClose(WebSocket webSocket, int statusCode, String reason) {
+                        System.out.println("웹소켓 세션이 종료되었습니다. : " + reason);
+                        return WebSocket.Listener.super.onClose(webSocket, statusCode, reason);
+                    }
+
+                    @Override
+                    public void onError(WebSocket webSocket, Throwable error) {
+                        System.err.println("예외 발생: " + error.getMessage());
+                    }
+                }).join();
+    }
+}
